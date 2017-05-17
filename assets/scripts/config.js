9087e9238548ddaf64f345b9a7d32ffea3274e7a
isharma-sharma <isharma.sharma55@gmail.com>
2017-05-17 00:21:59 -0400

    sign-in,sign-up,change-password done

---
+++ assets/scripts/config.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const config = {
+  apiOrigins: {
+    development: 'http://tic-tac-toe.wdibos.com',
+    production: 'https://aqueous-atoll-85096.herokuapp.com'
+  }
+}
+module.exports = config
---
+++ scripts/sign-up-json.sh
@@ -0,0 +1,13 @@
+curl "http://tic-tac-toe.wdibos.com/sign-up" \
+--include \
+--request POST \
+--header "Content-Type: application/json" \
+--data '{
+  "credentials" : {
+    "email" : "raja@gmail",
+    "password" : "12345",
+    "password_confirmation" : "12345"
+  }
+}'
+
+echo
