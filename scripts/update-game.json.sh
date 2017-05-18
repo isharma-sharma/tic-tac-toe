e60c4ac54a00984a354101ae04e9c84b4de3940c
isharma-sharma <isharma.sharma55@gmail.com>
2017-05-17 20:17:28 -0400

    forget this branch to commit

--- scripts/update-game.json.sh
+++ scripts/update-game.json.sh
@@ -5,15 +5,15 @@ curl "http://tic-tac-toe.wdibos.com/games/672" \
 --header "Authorization: Token token=BAhJIiUwNjQ2Y2RhMTA5ZGM4OWZkMjBkNTVlZDMxMTg3YjE4NAY6BkVG--a702276c99763f454d5d8746a6fba07e30fa4d23" \
 --data '{
   "games": {
-    "id": "602",
+    "id": 602,
     "cell": ["","","","","","","","",""],
     "over": false,
     "player_x":  {
-     "id": "102" ,
+     "id": 102 ,
      "email": "a@gmail"
    },
   "player_o": {
-    "id": "134",
+    "id": 134,
     "email":"b@gmail"
   }
   }
